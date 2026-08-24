20bb0718734f923ecf5661023df91fd5f739cef8
Abhinav Dayal <abhinavxdayal@gmail.com>
2026-08-24 20:10:09 +0530

    feat: initialize e6-lists project with Expo and create basic structure
    - Added package.json with dependencies and scripts for Expo project.
    - Created reset-project script to initialize project structure.
    - Implemented basic layout with _layout.tsx and index.tsx in src/app.
    - Developed Listviews component to display a list of items.
    - Added Divider component for UI separation.
    - Configured TypeScript with tsconfig.json for strict type checking.

---
+++ S6 Rn/E6 Lists/src/app/index.tsx
@@ -0,0 +1,7 @@
+import Listviews from "../components/Listviews";
+
+// Creating Index component
+export default function Index() {
+  // Returns Listviews component
+  return <Listviews />;
+}
---
+++ S6 Rn/E6 Lists/src/components/Listviews.tsx
@@ -0,0 +1,36 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+// Creating styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
+
+// Creating Listviews component
+const Listviews = () => {
+  // Returns list ui
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={[
+          { key: "Devin" },
+          { key: "Dan" },
+          { key: "Dominic" },
+          { key: "Jackson" },
+          { key: "Julie" },
+        ]}
+        renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
+      />
+    </View>
+  );
+};
+
+// Exporting our component
+export default Listviews;

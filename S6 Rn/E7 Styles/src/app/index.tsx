79abd152036967e809be0da01bcc3438e4694269
Abhinav Dayal <abhinavxdayal@gmail.com>
2026-09-01 11:56:29 -0300

    feat: add FixedDimensions and FlexDimensions components to index

--- S6 Rn/E7 Styles/src/app/index.tsx
+++ S6 Rn/E7 Styles/src/app/index.tsx
@@ -1,5 +1,7 @@
 import Divider from "../components/Divider";
 import LotsOfStyles from "../components/E7.1 TextStyling";
+import FixedDimensions from "../components/E7.2 FixedDimensions";
+import FlexDimensions from "../components/E7.3 FlexDimensions";
 
 // Creating Index component
 export default function Index() {
@@ -8,7 +10,13 @@ export default function Index() {
     <>
       <LotsOfStyles />
 
-      <Divider/>
+      <Divider />
+
+      <FixedDimensions />
+
+      <Divider />
+
+      <FlexDimensions />
     </>
   );
 }
